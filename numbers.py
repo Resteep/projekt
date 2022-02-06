d4c92c42f06a79f6eb96e3b3cfa0a6f1fc52f357
Resteep <jensggustafson@gmail.com>
2022-02-06 15:31:37 +0100

    num
    num

---
+++ numbers.py
@@ -0,0 +1,22 @@
+num = 6
+num2 = 7
+num3 = num + num2
+num4 = 7 * num3
+print('\n')
+print(num3)
+print()
+print((num3 * num2) + num4 * num2)
+print()
+print(num4)
+days_in_feb = 28
+print('\n')
+print(str(days_in_feb) + ' days in February')
+print()
+num = input(' enter your num ')
+num2 = input(' enter your num ')
+print(int(num) + int(num2))
+print()
+first_name = input('please enter your first name: ')
+last_name = input('please enter your last name: ')
+output = f'Hello, {first_name.capitalize()} {last_name.capitalize()}'
+print(output)
--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-#our code
-print('fuck you')
+#String Format
+from cmath import pi
+
+
+first_name = input('please enter your first name: ')
+last_name = input('please enter your last name: ')
+output = f'Hello, {first_name.capitalize()} {last_name.capitalize()}'
+print(output)
