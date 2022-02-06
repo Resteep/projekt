193db62581fc55d22241703c03085e31d19c6eca
Jens <99073732+Resteep@users.noreply.github.com>
2022-02-06 15:48:40 +0100

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -20,4 +20,4 @@ first_name = input('please enter your first name: ')
 last_name = input('please enter your last name: ')
 output = f'Hello, {first_name.capitalize()} {last_name.capitalize()}'
 print(output)
-print('Pappa')
+#print('Pappa')
