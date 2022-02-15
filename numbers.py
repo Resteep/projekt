d132d1f64939277091ded676df420976ce2e6329
Resteep <jensggustafson@gmail.com>
2022-02-15 19:32:28 +0100

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -16,18 +16,13 @@ num = input(' enter your num ')
 num2 = input(' enter your num ')
 print(int(num) + int(num2))
 print()
-<<<<<<< HEAD
 # first_name = input('please enter your first name: ')
 # last_name = input('please enter your last name: ')
 # output = f'Hello, {first_name.capitalize()} {last_name.capitalize()}'
 # print(output)
-=======
 first_name = input('please enter your first name: ')
 last_name = input('please enter your last name: ')
 output = f'Hello, {first_name.capitalize()} {last_name.capitalize()}'
 print(output)
 #print('Pappa')
-<<<<<<< HEAD
->>>>>>> 193db62581fc55d22241703c03085e31d19c6eca
-=======
->>>>>>> 193db62581fc55d22241703c03085e31d19c6eca
+
